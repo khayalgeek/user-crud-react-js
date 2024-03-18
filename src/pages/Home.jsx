1ef31428828b241026df9f3d918284238fda5746
khayal_997 <xeyal.binnetov@profit.az>
2024-03-18 11:29:01 -0700

    created home page and navbar component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import NavbarComp from "../components/Navbar";
+
+export default class Home extends Component{
+    
+
+    render(){
+       return <NavbarComp/>
+    }
+}
