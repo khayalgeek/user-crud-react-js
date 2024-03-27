5de081cca5a0e260ee50f890e7c76bf380019e6f
khayalgeek <xeyalbinnetov02@gmail.com>
2024-03-27 08:35:17 -0700

    change user list component function to class

--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -1,71 +1,79 @@
-import React from "react";
+import React, { Component } from "react";
 import { Table } from "reactstrap";
 import { DeleteBtn, EditBtn } from "../Buttons";
 
 const editUserById = () => {
-    alert('edit action');
+    alert(`edit action: ${id}`);
+
 };
 
 const deleteUserById = (id) => {
     alert(`delete action: ${id}`);
 };
 
-const UserList = ({ userList }) => {
-    return (
-        <Table
-            bordered
-            borderless
-            dark
-            hover
-            responsive
-            striped
-        >
-            <thead>
-                <tr>
-                    <th>
-                        #
-                    </th>
-                    <th>
-                        First Name
-                    </th>
-                    <th>
-                        Last Name
-                    </th>
-                    <th>
-                        Email
-                    </th>
-                    <th className="d-flex justify-content-center">
-                        <span>Operation...</span>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {userList.map(({ id, name, surname, email }) => {
-                    return (
-                        <tr key={id}>
-                            <th scope="row">
-                                {id}
-                            </th>
-                            <td>
-                                {name}
-                            </td>
-                            <td>
-                                {surname}
-                            </td>
-                            <td>
-                                {email}
-                            </td>
-                            <td className="d-flex justify-content-around">
-                                <EditBtn id={id} command={editUserById} />
-                                <DeleteBtn id={id} command={deleteUserById} />
-                            </td>
-                        </tr>
-                    );
-                })}
-
-            </tbody>
-        </Table>
-    )
+export default class UserList extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <Table
+                bordered
+                borderless
+                dark
+                hover
+                responsive
+                striped
+            >
+                <thead>
+                    <tr>
+                        <th>
+                            #
+                        </th>
+                        <th>
+                            First Name
+                        </th>
+                        <th>
+                            Last Name
+                        </th>
+                        <th>
+                            Email
+                        </th>
+                        <th className="d-flex justify-content-center">
+                            <span>Operation...</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.userList.map(({ id, name, surname, email }) => {
+                        return (
+                            <tr key={id}>
+                                <th scope="row">
+                                    {id}
+                                </th>
+                                <td>
+                                    {name}
+                                </td>
+                                <td>
+                                    {surname}
+                                </td>
+                                <td>
+                                    {email}
+                                </td>
+                                <td className="d-flex justify-content-around">
+                                    <EditBtn id={id} command={editUserById} />
+                                    <DeleteBtn id={id} command={deleteUserById} />
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </Table>
+        )
+    }
 }
+// const UserList = ({ userList }) => {
+    
+// }
 
-export default UserList;
+//export default UserList;
